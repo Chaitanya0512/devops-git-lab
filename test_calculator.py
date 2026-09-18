0ef1681dc9f3ab12b0365eb523ca0d67b40fa087
chaitanya vardhan <chaitanyavardhan24@gmail.com>
2026-09-18 09:22:23 +0000

    Use pytest in CI pipeline

--- test_calculator.py
+++ test_calculator.py
@@ -1,13 +1,7 @@
-import unittest
 from calculator import add, multiply
 
-class TestCalculator(unittest.TestCase):
+def test_add():
+    assert add(2, 3) == 5
 
-    def test_add(self):
-        self.assertEqual(add(2, 3), 5)
-
-    def test_multiply(self):
-        self.assertEqual(multiply(4, 5), 20)
-
-if __name__ == "__main__":
-    unittest.main()
+def test_multiply():
+    assert multiply(4, 5) == 20
